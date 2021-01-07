58c48e395217ce3ffcf7f158c76a5940e18746a6
Tommattih <76976994+Tommattih@users.noreply.github.com>
2021-01-07 19:14:25 -0300

    Calculadora de duas operações
    Uma experimento de calculadora que só executa soma e subtração.

---
+++ calculadoraExcludente.py
@@ -0,0 +1,12 @@
+numero = int(input('Insira um número '))
+outroNumero = int(input('Insira outro número '))
+operacao = input('Insira a operação ')
+
+if operacao == ('+'):
+	print(numero + outroNumero)
+if operacao == ('-'):
+	print(numero - outroNumero)
+if operacao == ('/'):
+	print('Operação inválida!')
+if operacao == ('*'):
+	print('Operação inválida!')
